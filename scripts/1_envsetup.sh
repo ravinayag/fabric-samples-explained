4ab9bdccc2ba65c3c94dd49c8762b476d2511c9b
ravi <ravi@lab.enactes.com>
2019-10-11 14:57:09 +0530

    nil

--- scripts/1_envsetup.sh
+++
@@ -1,7 +0,0 @@
-export PATH=${PWD}/../bin:${PWD}:$PATH
-export FABRIC_CFG_PATH=${PWD}
-
-export SYS_CHANNEL="mychannel"
-export CHANNEL_NAME=mychannel2
-export IMAGE_TAG=latest
-export COMPOSE_PROJECT_NAME=newnet
