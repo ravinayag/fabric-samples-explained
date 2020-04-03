66465b87c7ce483973ef350cb4c3eea26feb07e8
ravinayag <52193049+ravinayag@users.noreply.github.com>
2020-04-03 17:11:39 +0530

    Update 1_envsetup.sh
    changed syschannel name as per notes

--- scripts/1_envsetup.sh
+++ scripts/1_envsetup.sh
@@ -1,7 +1,7 @@
 export PATH=${PWD}/../bin:${PWD}:$PATH
 export FABRIC_CFG_PATH=${PWD}
 
-export SYS_CHANNEL="mychannel"
+export SYS_CHANNEL="syschannel"
 export CHANNEL_NAME=mychannel2
 export IMAGE_TAG=latest
 export COMPOSE_PROJECT_NAME=newnet
